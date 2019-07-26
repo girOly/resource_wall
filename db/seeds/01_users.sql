9af750621b5dd9c5a1322c653d271e316fa040d3
girOly <girardob@gmail.com>
2019-07-26 21:53:34 +0000

    Cleaned up Code

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,22 +2,22 @@ DELETE FROM users;
 
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Alice Smith', 'password', TRUE, 'alicesmith@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Alice Smith', 'password', 'alicesmith@gmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('John Michael', 'password', TRUE, 'johnmichael@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('John Michael', 'password', 'johnmichael@gmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Lauren Ipsum', 'password', TRUE, 'laurenipsum@hotmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Lauren Ipsum', 'password', 'laurenipsum@hotmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Guy Desjardin', 'password', TRUE, 'guy@live.com');
+INSERT INTO users (full_name, password, email) VALUES ('Guy Desjardin', 'password', 'guy@live.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Jannet Darette', 'password', FALSE, 'janet@hotmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Jannet Darette', 'password', 'janet@hotmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Petito Childo', 'password', FALSE, 'petito@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Petito Childo', 'password', 'petito@gmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Doette Man', 'password', TRUE, 'doette@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Doette Man', 'password', 'doette@gmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Jane Doe', 'password', FALSE, 'janedoe@hotmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Jane Doe', 'password', 'janedoe@hotmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Huawai Hayh', 'password', FALSE, 'huawai@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Huawai Hayh', 'password', 'huawai@gmail.com');
 
-INSERT INTO users (full_name, password, age, email) VALUES ('Michael Johnson', 'password', TRUE, 'mj@gmail.com');
+INSERT INTO users (full_name, password, email) VALUES ('Michael Johnson', 'password', 'mj@gmail.com');
--- db/seeds/03_categories.sql
+++ db/seeds/03_categories.sql
@@ -2,14 +2,11 @@ DELETE FROM categories;
 
 ALTER SEQUENCE categories_id_seq RESTART WITH 1;
 
-INSERT INTO categories (title, description, age)
-VALUES ('Sports', 'Anything sports related, no video games', false);
+INSERT INTO categories (title, description)
+VALUES ('Sports', 'Anything sports related, no video games');
 
-INSERT INTO categories (title, description, age)
-VALUES ('Video Games', 'Anything gaming related, no sports', false);
+INSERT INTO categories (title, description)
+VALUES ('Video Games', 'Anything gaming related, no sports');
 
-INSERT INTO categories (title, description, age)
-VALUES ('Tits', 'Anything tits related, no children', true);
-
-INSERT INTO categories (title, description, age)
-VALUES ('Food', 'For all your hungry needs', false);
+INSERT INTO categories (title, description)
+VALUES ('Food', 'For all your hungry needs)
