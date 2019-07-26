3ad25214cbb10d43c16dd83979fb86c7b8cb05ca
girOly <girardob@gmail.com>
2019-07-26 22:20:55 +0000

    Refactoring

--- db/seeds/06_resource_categories.sql
+++ db/seeds/06_resource_categories.sql
@@ -3,7 +3,7 @@ DELETE FROM resource_categories;
 ALTER SEQUENCE resource_categories_id_seq RESTART WITH 1;
 
 INSERT INTO resource_categories (resource_id, category_id)
-VALUES (1,4);
+VALUES (1,2);
 
 INSERT INTO resource_categories (resource_id, category_id)
 VALUES (2,3);
