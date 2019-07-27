61ce6a9affcfe8bff9d847e9e157218a8150103c
girOly <girardob@gmail.com>
2019-07-27 19:31:37 +0000

    Post JSON to homepage

---
+++ routes/resources.js
@@ -0,0 +1,23 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = db => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM resources;`)
+      .then(data => {
+        const resources = data.rows;
+        res.json({ resources });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
