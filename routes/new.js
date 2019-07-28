c1b1792d0a6649eb570f281da3ceb4baa3cfaa63
Duncan Haran <duncan.haran@gmail.com>
2019-07-28 16:13:38 +0000

    /new resource route added

--- routes/new.js
+++ routes/new.js
@@ -2,5 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
-  router.get("/")
+  router.get("/", (req, res) => {
+    res.render("../views/new")
+  });
+},
+
+db => {
+  router.post("/", (req, res) => {
+    const { external_url, thumbnail_url, description, title } = req.body
+    db.query(`
+    INSERT INTO resources (external_url, thumbnail_url, description, title)
+    VALUES ($1, $2, $3, $4);
+    `, [external_url, thumbnail_url, description, title])
+    .then(() => {
+      res.redirect("/")
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+  })
+})
+return router
 }
