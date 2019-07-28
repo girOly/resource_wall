dc392caac25aa70b8f18e18eb2d8f7ced817f9ea
Duncan Haran <duncan.haran@gmail.com>
2019-07-28 18:09:00 +0000

    register route moved around

--- routes/register.js
+++ routes/register.js
@@ -3,36 +3,33 @@ const router = express.Router();
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    res.render("../views/register")
+    res.render("register")
   });
-return router;
-},
 
-db => {
-router.post("/", (req, res) => {
-  const {email, password, full_name} = req.body
-  db.query(`
-  SELECT email
-  FROM users
-  WHERE email = $1;
-  `, [email])
-    .then(data => {
+  router.post("/", (req, res) => {
+    const { email, password, full_name } = req.body
+    db.query(`
+    SELECT email
+    FROM users
+    WHERE email = $1;
+    `, [email])
+      .then(data => {
       if (data) {
         //return email already in use try again please
       }
+      })
+      .done(db.query(`
+      INSERT INTO users (email, password, full_name)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    ` , [email, password, full_name])
+      .then(accInfo => {
+        res.redirect("/")
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
     })
-    .done(db.query(`
-    INSERT INTO users (email, password, full_name)
-    VALUES ($1, $2, $3)
-    RETURNING *;
-    `, [email, password, full_name])
-    .then(accInfo => {
-      res.redirect("/")
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-  })
-)
-return router
+  )
 })
+return router
 }
