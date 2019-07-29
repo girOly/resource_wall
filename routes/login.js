ca27d614a62cc6683b880f040ba7e59898f8b2df
Duncan Haran <duncan.haran@gmail.com>
2019-07-29 18:52:57 +0000

    user info sent with templatevars

--- routes/login.js
+++ routes/login.js
@@ -4,26 +4,22 @@ const router = express.Router();
 
 module.exports = db => {
 router.get("/", (req, res) => {
-  res.render("login")
+  res.render("login", {user:req.user})
 });
 
 router.post("/", (req, res) => {
   const {email, password} = req.body
-  console.log(req.body)
   db.query(`SELECT id, email, password
   FROM users
   WHERE email = $1
   AND password = $2;
   `, [email, password])
     .then(data => {
-      if (data.rows[0]) {
-        console.log(data.rows)
+      if (data) {
         req.session.user_id = data.rows[0].id
         res.redirect("/")
       } else {
-        res.send('shits broken')
-//         res.redirect("/login")
-// // try again message
+// try again message
       }
     })
     .catch(err => {
