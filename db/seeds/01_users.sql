fe4c1f4c09e8da705acbbf181635be05ceac26e3
girOly <girardob@gmail.com>
2019-07-29 20:34:14 +0000

    Implemented User Profile Pictures in db, linked userdata to profile page

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,22 +2,22 @@ DELETE FROM users;
 
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
-INSERT INTO users (full_name, password, email, bio) VALUES ('Alice Smith', 'password', 'alicesmith@gmail.com', 'my name is alice and I like apples');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Alice Smith', 'password', 'alicesmith@gmail.com', 'my name is Alice and I like apples', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('John Michael', 'password', 'johnmichael@gmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('John Michael', 'password', 'johnmichael@gmail.com','my name is John and I like lemons', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Lauren Ipsum', 'password', 'laurenipsum@hotmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Lauren Ipsum', 'password', 'laurenipsum@hotmail.com', 'my name is Lauren and I like oranges', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email, bio) VALUES ('Guy Desjardin', 'password', 'guy@live.com', 'who calls their kid guy, really');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Guy Desjardin', 'password', 'guy@live.com', 'who calls their kid guy, really', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Jannet Darette', 'password', 'janet@hotmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Jannet Darette', 'password', 'janet@hotmail.com','my name is Jannet and I like watermelon', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Petito Childo', 'password', 'petito@gmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Petito Childo', 'password', 'petito@gmail.com','my name is Petito and I like muffins', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Doette Man', 'password', 'doette@gmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Doette Man', 'password', 'doette@gmail.com', 'my name is Doette and I like scones', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Jane Doe', 'password', 'janedoe@hotmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Jane Doe', 'password', 'janedoe@hotmail.com', 'my name is Jane and I like bagels', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Huawai Hayh', 'password', 'huawai@gmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Huawai Hayh', 'password', 'huawai@gmail.com','my name is Huawai and I like pamplemousse', 'https://placekeanu.com/200/150');
 
-INSERT INTO users (full_name, password, email) VALUES ('Michael Johnson', 'password', 'mj@gmail.com');
+INSERT INTO users (full_name, password, email, bio, thumbnail_url) VALUES ('Michael Johnson', 'password', 'mj@gmail.com','my name is Michael and I like lamp', 'https://placekeanu.com/200/150');
