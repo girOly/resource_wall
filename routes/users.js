13fafb6f5d05f6976ae5bdeb07a54f316a840e18
Duncan Haran <duncan.haran@gmail.com>
2019-07-30 14:53:38 +0000

    edit user page uses cookie id not params

--- routes/users.js
+++ routes/users.js
@@ -2,79 +2,47 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.put("/:id/edit", (req, res) => {
+  router.put("/edit", (req, res) => {
     // will need to consolelog req.body when put button works
     const profileChanges = req.body
     const updateQuery = `
     UPDATE resources
-    SET external_url = $1, thumbnail_url = $2, description = $3, title = $4
+    SET external_url = $1, thumbnail_url = $2, bio = $3, title = $4
     WHERE id = $5
     RETURNING *;
     `
     if (req.user.id === req.params.id) {
-      db.query(updateQuery, profileChanges)
+      db.query(updateQuery, [profileChanges])
     }
   })
 
-  router.get("/:id/edit", (req, res) => {
-    const user_id = req.params.id
-    // TypeError: Cannot read property 'id' of undefined
-    if (req.user.id === user_id) {
+  router.get("/edit", (req, res) => {
+    const user_id = req.user.id
       db.query(`
       SELECT thumbnail_url, full_name, bio
       FROM users
       WHERE id = $1;
       `, [user_id])
-      .then((data) => {
-        const userData = data.rows[0]
-         res.render("users-edit", {userData, user:req.user})
-      })
-    } else {
-      db.query(`
-    SELECT thumbnail_url, full_name, bio
-    FROM users
-    WHERE id = $1;
-    `, [user_id])
-      .then((data) => {
-        const userData = data.rows[0]
-         res.render("users", {userData, user:req.user})
+      .then(() => {
+         res.render("users-edit", { user:req.user })
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
-    }
-})
+    });
 
-  router.get("/:id", (req, res) => {
-    const user_id = req.params.id
-    db.query(`
-    SELECT thumbnail_url, full_name, bio
-    FROM users
-    WHERE id = $1;
-    `, [user_id])
-      .then(data => {
-        const userData = data.rows[0];
-        res.render("users", {userData, user:req.user})
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/:id/liked", (req, res) => {
-    const user_id = req.params.id
-    db.query(`
-    SELECT resource_id, resources.external_url, resources.thumbnail_url, description, title
+router.get("/:id/liked", (req, res) => {
+  const user_id = req.params.id
+  db.query(`
+  SELECT resource_id, resources.external_url, resources.thumbnail_url, description, title
     FROM liked
     JOIN users ON user_id = users.id
     JOIN resources ON resource_id = resources.id
     WHERE users.id = $1;
     `, [user_id])
-      .then(data => {
+    .then(data => {
         const allResources = data.rows;
         res.render("resource-liked", { allResources, user:req.user });
       })
@@ -84,5 +52,23 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-  return router;
-}
+
+  router.get("/:id", (req, res) => {
+    const user_id = req.params.id
+    db.query(`
+    SELECT thumbnail_url, full_name, bio
+    FROM users
+    WHERE id = $1;
+    `, [user_id])
+    .then(data => {
+      const userData = data.rows[0];
+      res.render("users", {userData, user:req.user})
+    })
+    .catch(err => {
+      res
+          .status(500)
+          .json({ error: err.message });
+      });
+    });
+    return router;
+  }
