8effaa3dc70d0cacd241abe23950b3cfcedb8299
Duncan Haran <duncan.haran@gmail.com>
2019-08-01 18:58:08 +0000

    search function can search titles and descriptions

--- routes/resources.js
+++ routes/resources.js
@@ -4,27 +4,42 @@ const router = express.Router();
 router.use(methodOverride('_method'))
 
 module.exports = db => {
-  router.get("/new", (req, res) => {
-    if (req.user) {
-      db.query(`SELECT *
-        FROM categories;
-        `)
-        .then((data) => {
-          res.render("new", { user:req.user, categories:data.rows})
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-      });
-    }
-  });
+router.get("/new", (req, res) => {
+  if (req.user) {
+    db.query(`
+    SELECT *
+    FROM categories;
+    `)
+    .then((data) => {
+      res.render("new", { user:req.user, categories:data.rows})
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  }
+});
+
+router.get("/search", (req, res) => {
+  const { search } = req.query
+  db.query(`
+  SELECT *
+  FROM resources
+  WHERE description LIKE $1
+  OR title LIKE $1;
+  `, [`%${search}%`])
+  .then(data => {
+    res.render('home', { user:req.user, allResources:data.rows })
+  })
+  .catch(err => {
+    res.status(500).json({ error: err.message });
+  })
+})
 
 router.put("/:id/edit", (req, res) => {
   const resId = req.params.id
-    // will need to consolelog req.body when put button works
   const resChanges = req.body
-  console.log(resChanges);
   const findRes = `
     SELECT *
     FROM resources
@@ -54,113 +69,124 @@ router.put("/:id/edit", (req, res) => {
   })
 })
 
-  router.get("/:id/edit", (req, res) => {
-    const user_id = req.user.id
-    const resource_id = req.params.id
-    db.query(`
-    SELECT *
-    FROM resources
+router.get("/:id/edit", (req, res) => {
+  const user_id = req.user.id
+  const resource_id = req.params.id
+  db.query(`
+  SELECT *
+  FROM resources
+  WHERE id = $1;
+  `, [resource_id])
+  .then((resource) => {
+    if (user_id !== resource.rows[0].created_by) {
+      res.redirect(`/${resource_id}`)
+    } else {
+      db.query(`
+      SELECT *
+      FROM categories;
+      `)
+      .then((cats) => {
+        let { categories } = cats.rows
+        res.render(`resources-edit`, { user:req.user, resource:resource.rows[0], categories })
+      })
+    }
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  })
+})
+
+router.get("/:id", (req, res) => {
+  const resource_id = req.params.id
+  db.query(`
+  SELECT r.*, c.date_created, c.content, u.full_name
+  FROM resources r
+  LEFT JOIN comments c ON c.resource_id = r.id
+  LEFT JOIN users u ON c.user_id = u.id
+  WHERE r.id = $1;
+  `, [resource_id])
+  .then(data => {
+    const resource = data.rows;
+    res.render("resource",{ resource, user: req.user });
+  })
+  .catch(err => {
+    res.status(500).json({ error: err.message });
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const resource_id = req.params.id
+  const { external_url, description, title, thumbnail_url } = req.body
+  db.query(`
+    UPDATE resources
+    SET external_url = $1, thumbnail_url = $2, description = $3, title = $4
+    WHERE id = $5
+    RETURNING *;
+    `, [external_url, thumbnail_url, description, title, resource_id])
+  .then(data => {
+    res.redirect(`resources/${resource_id}`);
+  })
+  .catch(err => {
+    res.status(500).json({ error: err.message });
+  });
+})
+
+router.delete("/:id", (req, res) => {
+  const resource_id = req.params.id
+  db.query(`
+    DELETE FROM resources
     WHERE id = $1;
     `, [resource_id])
-    .then((resource) => {
-      if (user_id !== resource.rows[0].created_by) {
-        res.redirect(`/${resource_id}`)
-      } else {
-        db.query(`
-        SELECT *
-        FROM categories;
-        `)
-        .then((cats) => {
-          let { categories } = cats.rows
-          res.render(`resources-edit`, { user:req.user, resource:resource.rows[0], categories })
-        })
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    })
+  .then(() => {
+    res.redirect("/");
   })
-
-  router.get("/:id", (req, res) => {
-    const resource_id = req.params.id
-    db.query(`
-    SELECT r.*, c.date_created, c.content, u.full_name
-    FROM resources r
-    LEFT JOIN comments c ON c.resource_id = r.id
-    LEFT JOIN users u ON c.user_id = u.id
-    WHERE r.id = $1;
-    `, [resource_id])
-    .then(data => {
-      const resource = data.rows;
-      console.log(resource)
-      res.render("resource",{ resource, user: req.user });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
+  .catch(err => {
+    res.status(500).json({ error: err.message });
   });
+})
 
-  router.put("/:id", (req, res) => {
-    const resource_id = req.params.id
-    const { external_url, description, title, thumbnail_url } = req.body
-    db.query(`
-      UPDATE resources
-      SET external_url = $1, thumbnail_url = $2, description = $3, title = $4
-      WHERE id = $5
-      RETURNING *;
-      `, [external_url, thumbnail_url, description, title, resource_id])
-    .then(data => {
-      res.redirect(`resources/${resource_id}`);
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
-  })
-
-  router.delete("/:id", (req, res) => {
-    const resource_id = req.params.id
-    db.query(`
-      DELETE FROM resources
-      WHERE id = $1;
-      `, [resource_id])
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
-  })
-
-
-  router.post("/new", (req, res) => {
-    console.log(req.body)
-    const { external_url, thumbnail_url, description, title, categories } = req.body
-    db.query(`
-    INSERT INTO resources (external_url, thumbnail_url, description, title, created_by)
-    VALUES ($1, $2, $3, $4, $5)
-    RETURNING *;
-    `, [external_url, thumbnail_url, description, title, req.user.id])
-    .then((data) => {
-      console.log(data)
-      const resource_id = data.rows[0].id
-      let queryArr = [];
-      for (let cat of categories) {
-        queryArr.push(db.query(`
-          INSERT INTO resource_categories (category_id, resource_id)
-          VALUES ($1, $2);
-          `, [cat, resource_id]))
-        }
-        Promise.all(queryArr)
-        .then(() => {
-          res.redirect(`/resources/${resource_id}`)
-        })
-        .catch(err => {
-          res.status(500).json({ error: err.message });
-        })
+router.post("/new", (req, res) => {
+  const { external_url, thumbnail_url, description, title, categories } = req.body
+  db.query(`
+  INSERT INTO resources (external_url, thumbnail_url, description, title, created_by)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;
+  `, [external_url, thumbnail_url, description, title, req.user.id])
+  .then((data) => {
+    const resource_id = data.rows[0].id
+    let queryArr = [];
+    for (let cat of categories) {
+      queryArr.push(db.query(`
+        INSERT INTO resource_categories (category_id, resource_id)
+        VALUES ($1, $2);
+        `, [cat, resource_id]))
+      }
+      Promise.all(queryArr)
+      .then(() => {
+        res.redirect(`/resources/${resource_id}`)
       })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      })
+  })
+})
 
-    })
-    return router;
-  }
+router.post("/bookmarked/:id", (req, res) => {
+  const resource_id = req.params.id
+  const user_id = req.user.id
+  db.query(`
+  INSERT INTO bookmarked (resource_id, user_id)
+  VALUES ($1, $2)
+  `, [resource_id, user_id])
+  .then(() => {
+    res.redirect(`/resources/${resource_id}`)
+  })
+  .catch(err => {
+    res.status(500).json({ error: err.message });
+  })
+})
+
+  return router;
+}
